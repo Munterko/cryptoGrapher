677df98cd3b39c2bfaf7696f45af740621dae5a9
Maciej Chabinowski <mchabinowski@gmail.com>
2021-08-18 23:21:36 +0200

    Updated dataDrabAndPlot.py to allow for more graph customization

--- dataGrabAndPlot.py
+++ dataGrabAndPlot.py
@@ -9,13 +9,15 @@ def safeInt(number, safeMinimum=1):
         r = safeMinimum
     return r
 
-def getCryptoGraph(startDate, endDate, ticker):
+def getCryptoGraph(ticker, startDate, endDate, chartType= 'candle', tradVolume=True):
     startDate = (str(startDate) + "---").split('-')
-    start_date = datetime(safeInt(startDate[0],2019), safeInt(startDate[1]), safeInt(startDate[2]))
     endDate = (str(endDate) + "---").split('-')
+
+    start_date = datetime(safeInt(startDate[0],2019), safeInt(startDate[1]), safeInt(startDate[2]))
     end_date = datetime(safeInt(endDate[0],2020), safeInt(endDate[1]), safeInt(endDate[2]))
-    data = yfinance.download(f'{ticker}-USD', start=start_date, end=end_date)
-    mplfinance.plot(data,type='candle',mav=(3,6,9),volume=True,show_nontrading=True)
+
+    data = yfinance.download(f'{ticker}-USD', start= start_date, end= end_date)
+    mplfinance.plot(data, type= chartType, mav= (3,6,9),volume= tradVolume, show_nontrading= True)
 
 
 
