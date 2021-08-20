66ca74f01869027d63eef568e053214a85e68b3d
Maciej Chabinowski <mchabinowski@gmail.com>
2021-08-20 21:31:29 +0200

    Layout major rework

--- grapherApp.py
+++ grapherApp.py
@@ -1,54 +1,93 @@
-from tkinter import *   
+from tkinter import *
 from tkinter import ttk, messagebox
 from dataGrabAndPlot import getCryptoGraph
 
 
 class cryptoGrapher:
     def __init__(self, master):
-        '''
-        Date inputs to be changed for more practical input object types such as LabeledScale or Spinbox.
-        '''
+        master.title('cryptoGrapher')
+        #master.iconbitmap('/') #set bugRepWindow icon
+        master.geometry(f'{master.winfo_screenwidth()}x{master.winfo_screenheight()}')
+        master.minsize(1250,200)
 
-        #Title and description
-        ttk.Label(master, text = "crypthGrapher", background='dark blue', foreground='white').grid(column=2, row=1)
-        ttk.Label(master, text= 'This is a GUI aplication that creates custom graphs of crypto prices. Dates should be provided either as a year or in a YYYY-MM-DD format.', wraplength= 200, justify= CENTER).grid(column=2, row=2)
+        head = ttk.Frame(master)
+        body = ttk.Frame(master, relief=SUNKEN)
+        graph = ttk.Frame(master, relief=SUNKEN)
+        foot = ttk.Frame(master, relief=RIDGE, height=200, width=100)
+        # graphTools = ttk.Frame(master)
         
-        #Ticker text field
-        ttk.Label(master, text="Ticker").grid(column= 1, row=3)
+        head.place(relx=0, y=0, relwidth=1, height=35)
+        body.place(relx=0,y=35, relwidth=1, height=60)
+        graph.place(relx=0, y=95, relwidth=1, relheight=1, height=-190)
+        foot.place(relx=0, rely=1, y=-95, relwidth=1, height=95)
+        
+
+        ttk.Label(head, text = 'This is a GUI aplication that creates custom graphs of crypto prices. Dates should be provided either as a year or in a YYYY-MM-DD format.').pack(side = LEFT, anchor=W, padx=20, expand=True)#f'{self.ticker} from {self.startDate} to {self.endDate}').pack() 
+        self.fscreen = BooleanVar()
+        ttk.Button(head, text = 'Fullscreen mode', command= lambda: self.fullscreenMode(master)).pack(side = RIGHT, anchor=E, padx=20, expand=False)
+        ttk.Button(head, text = 'Report a bug', command=lambda: self.bugRep(master)). pack(side = RIGHT, anchor=E, expand=False)
+
+
+        # Variable controlling layout details of all elements in the Body Frame
+        lBodyLayout = {'side':LEFT, 'padx':20}
+        rBodyLayout = {'side':RIGHT, 'padx':20, 'ipadx': 10, 'ipady':10}
+
+
+        tickerField = Frame(body)
+        tickerField.pack(lBodyLayout)
+        ttk.Label(tickerField, text="Ticker").pack()
         self.ticker = StringVar()
-        ttk.Entry(master, textvariable= self.ticker, justify= CENTER).grid(column= 1, row=4)
+        ttk.Entry(tickerField, textvariable= self.ticker, justify= CENTER).pack()
         self.ticker.set('ETH')      #Displays ETH as an default value in the entry field
 
-        #Start-date text field
-        ttk.Label(master, text="Beginning date", justify= CENTER).grid(column= 2, row=3)
-        self.startDate = ttk.Entry(master)
-        self.startDate.grid(column= 2, row=4)
+        startDateField = Frame(body)
+        startDateField.pack(lBodyLayout)
+        ttk.Label(startDateField, text="Beginning date", justify= CENTER).pack()
+        self.startDate = ttk.Entry(startDateField)
+        self.startDate.pack()
         
-        #End-date text field
-        ttk.Label(master, text="End date", justify= CENTER).grid(column= 3, row=3)
-        self.endDate = ttk.Entry(master)
-        self.endDate.grid(column= 3, row=4)
+        endDateField = Frame(body)
+        endDateField.pack(lBodyLayout)
+        ttk.Label(endDateField, text="End date", justify= CENTER).pack()
+        self.endDate = ttk.Entry(endDateField)
+        self.endDate.pack()
         
-        #Trading-volume check button
-        self.volume = BooleanVar()
-        ttk.Checkbutton(master, text="Trading volume", variable=self.volume, onvalue=True, offvalue=False).grid(column=2, row=5)
-        self.volume.set(True)
-
-        #Drop-down char type selection
-        ttk.Label(master, text="Char type").grid(column= 1, row= 5)
+        dropDownField = Frame(body)
+        dropDownField.pack(lBodyLayout)
+        ttk.Label(dropDownField, text="Char type").pack()
         self.charType = StringVar()
-        ttk.Combobox(master, values=['line', 'candle', 'ohlc', 'pnf', 'renko'], validate= 'all', textvariable= self.charType).grid(column= 1, row= 6)
+        ttk.Combobox(dropDownField, values=['line', 'candle', 'ohlc', 'pnf', 'renko'], validate= 'all', textvariable= self.charType).pack()
         self.charType.set('line')
 
-        # #Debugging button
-        # self.check = ttk.Button(master, text='debug', command= lambda: messagebox.showinfo("status", self.ticker.get())).grid(column=3, row=5)
-    
-        #Buttons in the last row
-        ttk.Button(master, text = 'App status', command= lambda: messagebox.showinfo('App status', "version 0.2 - those graphs will get fancy one day!")).grid(column=1, row=7)
-        ttk.Button(master, text = 'Create graph', command= lambda: getCryptoGraph(self.ticker.get(), self.startDate.get(),self.endDate.get(), tradVolume= self.volume.get(), chartType= self.charType.get() )).grid(column=2, row=7)
-        ttk.Button(master, text = 'Quit', command= master.destroy).grid(column= 3, row=7)
-        
+        self.volume = BooleanVar()
+        ttk.Checkbutton(body, text="Trading volume", variable=self.volume, onvalue=True, offvalue=False).pack(lBodyLayout)
+        self.volume.set(True)
 
+
+        ttk.Button(body, text = 'Quit', command= master.destroy).pack(rBodyLayout)
+        ttk.Button(body, text = 'App status', command= lambda: messagebox.showinfo('App status', "version 0.3 - Ugly layout, but a layout!")).pack(rBodyLayout)
+        ttk.Button(body, text = 'Create graph', command= lambda: getCryptoGraph(self.ticker.get(), self.startDate.get(),self.endDate.get(), tradVolume= self.volume.get(), chartType= self.charType.get() )).pack(rBodyLayout)
+        
+        ttk.Label(graph, text= 'Graph placeholder').pack(side=TOP)
+        ttk.Label(graph, text= 'Graph placeholder').pack(side=BOTTOM)
+        ttk.Label(foot, text= 'Foot placeholder').pack(side=TOP)
+        ttk.Label(foot, text= 'Foot placeholder').pack(side=BOTTOM)
+
+    def fullscreenMode(self, master):
+        pass
+    
+
+    def bugRep(self, master):
+        bugRepWindow = Toplevel(master)
+        bugRepWindow.title('Report a bug')
+        bugRepWindow.geometry('350x250')
+        ttk.Label(bugRepWindow, text= 'Not jet implemented').pack(anchor=CENTER, pady=75)
+        ttk.Button(bugRepWindow, text= 'Close', command= bugRepWindow.destroy).pack(anchor=SE, side=BOTTOM, padx=5, pady=10)
+        '''
+        To be written
+        '''
+
+        
     def validate():
         #Validates user inputs like dates or char type and produces a message box if there is a problem
         pass
